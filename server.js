c01e162e479253ff05f91a7722cb7f82a3bdbd64
zakariye <zakariye1942@icloud.com>
2024-02-14 13:39:47 +0100

    example

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const PORT = process.env.PORT || 8082;
+const PORT = process.env.PORT || 3001;
 const express = require("express");
 const app = express();
 
@@ -9,17 +9,32 @@ const bodyParser = require("body-parser");
 const cors = require("cors"); 
 const morgan = require("morgan");
 
+var corsOptions = {
+  origin: '*', 
+  
+};
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(helmet());
-app.use(cors()); 
+app.use(cors(corsOptions)); 
 
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.json({ user: "logic backend" });
 });
+
+
+app.post('/location', (req, res) => {
+  const { latitude, longitude } = req.body;
+ console.log(`Received location: Latitude ${latitude}, Longitude ${longitude}`);
+
+  res.status(200).json({ message: 'Location received successfully.' });
+});
+
 
 app.listen(PORT, () => {
   console.log("Server is listening on port " + PORT);
