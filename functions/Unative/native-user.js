a404387aca08ad807b00213690c740008998254f
zakariye <zakariye1942@icloud.com>
2024-04-02 13:55:30 +0200

    updated ride request

--- functions/Unative/native-user.js
+++ functions/Unative/native-user.js
@@ -1,18 +1,16 @@
-const Ride = require ('../model/Ride.js');
-const User = require ('../model/User.js');
+const Ride = require('../model/Ride.js');
+const User = require('../model/User.js');
 const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const JWT_SECRET = process.env.JWT_SECRET;
 require('dotenv').config();
 
 
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET;
 
-
-async function RideRequest (req,res) {
-    const userId = req.user.id; 
+async function RideRequest(req, res) {
+    const userId = req.user.id;
     const { pickup_latitude, pickup_longitude, dropoff_latitude, dropoff_longitude, fare } = req.body;
     try {
-
         const user = await User.findById(userId);
         if (!user) {
             return res.status(404).send('User not found');
@@ -25,18 +23,16 @@ async function RideRequest (req,res) {
             pickup_longitude,
             dropoff_latitude,
             dropoff_longitude,
-            user_proposed_fare: fare
+            user_proposed_fare: fare,
+            fare_status: 'proposed' 
         });
 
-
         await ride.save();
-
         res.status(201).json({ message: 'Ride requested successfully', rideId: ride._id });
     } catch (error) {
         console.error('Database Error:', error);
         res.status(500).send('Server Error');
     }
+}
 
-};
-
-module.exports = {RideRequest};
+module.exports = { RideRequest };
